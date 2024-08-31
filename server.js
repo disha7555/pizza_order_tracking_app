5b54ba954ea7789999cb3e0820a95d32cd5faad4
disha7555 <190320107555.ce.disha.shah@gmailcom>
2024-08-31 11:12:46 +0530

    frontend pages

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const expressLayout=require('express-ejs-layouts');
 const PORT=process.env.PORT||3300;
 const path=require('path');
 
-app.get('/',(req,res)=>{
-    res.render('home');
-});
 //set assets
 app.use(express.static('public'));
+
 
 //set template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 //app.set('views',path.join(__dirname,'/resources/scss'));
 app.set('view engine','ejs');
+
+require('./routes/web')(app); //as it imports a function
 
 
 app.listen(PORT,()=>{
