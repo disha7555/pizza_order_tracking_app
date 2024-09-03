fd5542c968a72cd7905a51b5a782173fd56ed2bb
disha7555 <190320107555.ce.disha.shah@gmailcom>
2024-09-03 16:53:49 +0530

    new changes

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,3 +1,4 @@
+const User=require("../../models/user");
 function authController(){
     return{
         login(req,res){
---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:Strin,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'customer'
+    }
+},
+{timestamps:true}
+);
+
+const User=new mongoose.model("User",UserSchema);
+
+module.exports=User;
